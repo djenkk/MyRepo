51f4c6cab44210330ac0d8b48ef1c2393487f15a
stefan <stefan@DESKTOP-3V0KA9D>
2021-03-19 21:04:02 +0200

    gotovo

--- ddd/src/ddd/Class.java
+++ ddd/src/ddd/Class.java
@@ -1,9 +1,22 @@
 package ddd;
 
+import java.util.Scanner;
+
 public class Class {
 
 	public static void main(String[] args) {
-System.out.print("Hello world");
+System.out.print("Hello again ");
 	}
 
 }
+
+
+public static void main(String[] args) {
+System.out.print("Hello again ");
+}
+
+public static void main(String[] args) {
+System.out.print("Hello again ");
+}
+
+
